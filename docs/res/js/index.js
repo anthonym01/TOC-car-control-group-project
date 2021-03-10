4ceca9344b237a1bfc481fef58e5313e78053ef2
Samuel A. Matheson <57382365+anthonym01@users.noreply.github.com>
2021-03-09 19:38:26 -0500

    control state

--- docs/res/js/index.js
+++ docs/res/js/index.js
@@ -1,78 +1,227 @@
+const stabput = document.getElementById('stabput')
+const stateout = document.getElementById('current_state')
 
-//strip variables from links
-function get_url_variables(url) {//Gets url variables as an object
-    if (url == undefined) { url = window.location.href }
-    //Yoinked from
-    //https://gomakethings.com/getting-all-query-string-values-from-a-url-with-vanilla-js/
-    var params = {};
-    var parser = document.createElement('a');
-    parser.href = url;
-    var query = parser.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        params[pair[0]] = decodeURIComponent(pair[1]);
-    }
-    return params;
-    //returns Object { "": "undefined" } if empty
-    //Call with var this_url = getParams(window.location.href);
+window.addEventListener('load', function () {
+    /*console.log('
+    ⣿⣿⣻⣽⡿⣿⣎⠙⣿⣞⣷⡌⢻⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⡄⠹⣿⣿⡆⠻⣿⣟⣯⡿⣽⡿⣿⣿⣿⣿⣽⡷⣯⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣻⣽⡿⣿⣎⠙⣿⣞⣷⡌⢻⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⡄⠹⣿⣿⡆⠻⣿⣟⣯⡿⣽⡿⣿⣿⣿⣿⣽⡷⣯⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣟⣷⣿⣿⣿⡀⠹⣟⣾⣟⣆⠹⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢠⡘⣿⣿⡄⠉⢿⣿⣽⡷⣿⣻⣿⣿⣿⣿⡝⣷⣯⢿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣯⢿⣾⢿⣿⡄⢄⠘⢿⣞⡿⣧⡈⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣧⠘⣿⣷⠈⣦⠙⢿⣽⣷⣻⣽⣿⣿⣿⣿⣌⢿⣯⢿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣟⣯⣿⢿⣿⡆⢸⡷⡈⢻⡽⣷⡷⡄⠻⣽⣿⣿⡿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣏⢰⣯⢷⠈⣿⡆⢹⢷⡌⠻⡾⢋⣱⣯⣿⣿⣿⣿⡆⢻⡿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⡎⣿⢾⡿⣿⡆⢸⣽⢻⣄⠹⣷⣟⣿⣄⠹⣟⣿⣿⣟⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⢸⣯⣟⣧⠘⣷⠈⡯⠛⢀⡐⢾⣟⣷⣻⣿⣿⣿⡿⡌⢿⣻⣿⣿⣿
+    ⣿⣿⣿⣿⣧⢸⡿⣟⣿⡇⢸⣯⣟⣮⢧⡈⢿⣞⡿⣦⠘⠏⣹⣿⣽⢿⣿⣿⣿⣿⣯⣿⣿⣿⡇⢸⣿⣿⣾⡆⠹⢀⣠⣾⣟⣷⡈⢿⣞⣯⢿⣿⣿⣿⢷⠘⣯⣿⣿⣿
+    ⣿⣿⣿⣿⣿⡈⣿⢿⣽⡇⠘⠛⠛⠛⠓⠓⠈⠛⠛⠟⠇⢀⢿⣻⣿⣯⢿⣿⣿⣿⣷⢿⣿⣿⠁⣾⣿⣿⣿⣧⡄⠇⣹⣿⣾⣯⣿⡄⠻⣽⣯⢿⣻⣿⣿⡇⢹⣿⣿⣿
+    ⣿⣿⣿⣿⣿⡇⢹⣿⡽⡇⢸⣿⣿⣿⣿⣿⣞⣆⠰⣶⣶⡄⢀⢻⡿⣯⣿⡽⣿⣿⣿⢯⣟⡿⢀⣿⣿⣿⣿⣿⣧⠐⣸⣿⣿⣷⣿⣿⣆⠹⣯⣿⣻⣿⣿⣿⢀⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⠘⣯⡿⡇⢸⣿⣿⣿⣿⣿⣿⣿⣧⡈⢿⣳⠘⡄⠻⣿⢾⣽⣟⡿⣿⢯⣿⡇⢸⣿⣿⣿⣿⣿⣿⡀⢾⣿⣿⣿⣿⣿⣿⣆⠹⣾⣷⣻⣿⡿⡇⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⡇⢹⣿⠇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠻⡇⢹⣆⠹⣟⣾⣽⣻⣟⣿⣽⠁⣾⣿⣿⣿⣿⣿⣿⣇⣿⣿⠿⠛⠛⠉⠙⠋⢀⠁⢘⣯⣿⣿⣧⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⡈⣿⡃⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡙⠌⣿⣆⠘⣿⣞⡿⣞⡿⡞⢠⣿⣿⣿⣿⣿⡿⠛⠉⠁⢀⣀⣠⣤⣤⣶⣶⣶⡆⢻⣽⣞⡿⣷⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⡿⠃⠘⠁⠉⠉⠉⠉⠉⠉⠉⠉⠉⠙⠛⠛⢿⣄⢻⣿⣧⠘⢯⣟⡿⣽⠁⣾⣿⣿⣿⣿⣿⡃⢀⢀⠘⠛⠿⢿⣻⣟⣯⣽⣻⣵⡀⢿⣯⣟⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣶⣶⡆⢀⣿⣾⣿⣾⣷⣿⣶⠿⠚⠉⢀⢀⣤⣿⣷⣿⣿⣷⡈⢿⣻⢃⣼⣿⣿⣿⣿⣻⣿⣿⣿⡶⣦⣤⣄⣀⡀⠉⠛⠛⠷⣯⣳⠈⣾⡽⣾⣿⣿⣿
+    ⣿⣿⣻⣿⣿⣿⣿⣿⡿⠐⣿⣿⣿⣿⠿⠋⠁⢀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣌⣥⣾⡿⣿⣿⣷⣿⣿⢿⣷⣿⣿⣟⣾⣽⣳⢯⣟⣶⣦⣤⡾⣟⣦⠘⣿⢾⣿⣿⣿
+    ⣿⣿⡷⣿⣿⣿⣿⣿⡗⣦⠸⡿⠋⠁⢀⢀⣠⣴⢿⣿⣽⣻⢽⣾⣟⣷⣿⣟⣿⣿⣿⣳⠿⣵⣧⣼⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣽⣳⣯⣿⣿⣿⣽⢀⢷⣻⣿⣿⣿
+    ⣿⣿⣿⣷⣻⣿⣿⣿⡷⠛⣁⢀⣀⣤⣶⣿⣛⡿⣿⣮⣽⡻⣿⣮⣽⣻⢯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢀⢸⣿⣿⣿⣿
+    ⣿⣿⣿⣷⢿⣽⣿⣿⣷⣿⣷⣆⠹⣿⣶⣯⠿⣿⣶⣟⣻⢿⣷⣽⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣯⣟⣿⣿⣿
+    ⣿⣿⣻⣿⣿⢾⡽⣿⣿⣿⣿⣿⣆⢹⣶⣿⣻⣷⣯⣟⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢀⡿⡇⣿⣿⣿
+    ⣿⣿⡻⣽⣿⣯⢿⣽⣻⣿⣿⣿⣿⣆⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢸⣿⠇⣿⣿⣿
+    ⣿⣿⣿⣦⠛⣿⢯⣷⢿⡽⣿⣿⣿⣿⣆⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠎⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣿⣾⣣⣿⣿⣿
+    ⣿⣿⠙⣿⣿⣌⠻⣽⢯⣿⣽⣻⣿⣿⣿⣧⠩⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢰⢣⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⢀⢀⢿⣞⣷⣿⣿⣿
+    ⣿⣿⣧⠘⣿⣿⡷⣌⠙⢷⣯⡷⣟⣿⣿⣿⣷⡀⡹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣈⠃⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢀⣴⡧⢀⠸⣿⡽⣿⣿⣿
+    ⣿⣿⢻⣷⡈⢿⣿⣿⢧⢀⠙⢿⣻⡾⣽⣻⣿⣿⣄⠌⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⢁⣰⣾⣟⡿⢀⡄⢿⣟⣿⣿⣿
+    ⣿⣿⢀⠹⣟⣆⠻⣿⣿⣆⢀⣀⠉⠻⣿⡽⣯⣿⣿⣷⣈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⢀⣠⠘⣯⣷⣿⡟⢀⢆⠸⣿⣿⣿⣿
+    ⣿⣿⣇⢱⡘⢿⣷⣬⣙⠿⣧⠘⣆⢀⠈⠻⣷⣟⣾⢿⣿⣆⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣠⡞⢡⣿⢀⣿⣿⣿⠇⡄⢸⡄⢻⣿⣿⣿
+    ⣿⣿⣿⡆⢣⡀⠙⢾⣟⣿⣿⣷⡈⠂⠘⣦⡈⠿⣯⣿⢾⣿⣆⠙⠻⠿⠿⠿⠿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢋⣠⣾⡟⢠⣿⣿⢀⣿⣿⡟⢠⣿⢈⣧⠘⣿⣿⣿
+    ⣿⣿⠻⣿⡄⢳⡄⢆⡙⠾⣽⣿⣿⣆⡀⢹⡷⣄⠙⢿⣿⡾⣿⣆⢀⡀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣀⣠⣴⡿⣯⠏⣠⣿⣿⡏⢸⣿⡿⢁⣿⣿⢀⣿⠆⣿⣿⣿
+    ⣿⣿⣦⡙⣿⣆⢻⡌⢿⣶⢤⣉⣙⣿⣷⡀⠙⠽⠷⠄⠹⣿⣟⣿⣆⢙⣋⣤⣤⣤⣄⣀⢀⢀⢀⢀⣾⣿⣟⡷⣯⡿⢃⣼⣿⣿⣿⠇⣼⡟⣡⣿⣿⣿⢀⡿⢠⣿⣿⣿
+    ⣿⣿⣿⣷⣮⣿⣿⣿⡌⠁⢤⣤⣤⣤⣬⣭⣴⣶⣶⣶⣆⠈⢻⣿⣿⣆⢻⣿⣿⣿⣿⣿⣿⣷⣶⣤⣌⣉⡘⠛⠻⠶⣿⣿⣿⣿⡟⣰⣫⣴⣿⣿⣿⣿⠄⣷⣿⣿⣿⣿
+    ');*/
+});
+
+document.getElementById('arow').addEventListener('click', function () { stabput.value = stabput.value + 'a,' })
+document.getElementById('brow').addEventListener('click', function () { stabput.value = stabput.value + 'b,' })
+document.getElementById('crow').addEventListener('click', function () { stabput.value = stabput.value + 'c,' })
+document.getElementById('drow').addEventListener('click', function () { stabput.value = stabput.value + 'd,' })
+document.getElementById('erow').addEventListener('click', function () { stabput.value = stabput.value + 'e,' })
+document.getElementById('frow').addEventListener('click', function () { stabput.value = stabput.value + 'f,' })
+document.getElementById('grow').addEventListener('click', function () { stabput.value = stabput.value + 'g,' })
+//document.getElementById('hrow').addEventListener('click', function () { stabput.value = stabput.value + 'h,' })
+document.getElementById('irow').addEventListener('click', function () { stabput.value = stabput.value + 'i,' })
+//document.getElementById('jrow').addEventListener('click', function () { stabput.value = stabput.value + 'j,' })
+
+let inptarray = [];
+
+let delay = 1000;//in miliseconds
+
+let states = [
+    "Error",//0
+    "Off",//1
+    "ignition-on",//2
+    "Hold",//3
+    "engine-started",//4
+    "stationary position",//5
+    "in-reverse-motion",//6
+    "in-forward-motion",//7
+    "cruise-control-engaged"//8
+]
+
+let current_state = 1;
+let speed = 0; /* IMPLIMENT SPEED */
+
+document.getElementById('test_run').addEventListener('click', pullupinput)
+
+function pullupinput() {
+    current_state = 1
+    speed = 0;
+    inptarray = [];
+    raw_input = stabput.value.toLowerCase();
+    console.log('Raw input: ', raw_input)
+
+    inptarray = raw_input.split(',')
+
+    console.log(inptarray)
+    process_input()
 }
 
-async function clipboard(textpush) {
-    copyText.toString()
-    var temptxtbox = document.createElement("input")
-    document.body.appendChild(temptxtbox)
-    temptxtbox.value = textpush
-    temptxtbox.select()
-    document.execCommand("copy")
-    document.body.removeChild(temptxtbox)
-}
+function process_input() {
+    for (let i in inptarray) {
+        console.log('current state: ', states[current_state], ' Input/signal: ', inptarray[i])
+        switch (inptarray[i]) {
+            case ""://empty string
+                console.warn(states[current_state], ' no action can be taken with signal ""')
+                failiure(states[current_state], "")
+                return -1;
+                break;
+            case "a"://start signal
+                switch (current_state) {
+                    case 1: current_state = 2; console.log('Off -> ignition-on'); break;//Off -> ignition-on
+                    case 2: current_state = 1; console.log('ignition-on -> Off'); break;//ignition-on -> Off
+                    case 3: current_state = 4; console.log('Hold -> engine-started'); break;//Hold -> engine-started
+                    case 4: current_state = 1; console.log('engine-started -> Off'); break;//engine-started -> Off
+                    default:
+                        console.warn(states[current_state], ' no action can be taken with signal "a"');
+                        failiure(states[current_state], "a")
+                        return -1;
+                }
+                break;
 
-let rand = {//random numbers
-    HEX: function () { return '#' + Math.floor(Math.random() * 16777215).toString(16) },
-    RGB: function () { return { RED: this.number(255, 0), GREEN: this.number(255, 0), BLUE: this.number(255, 0) } },
-    HSL: function () { return { HUE: this.number(360, 0), SATURATION: this.number(100, 0) + '%', LIGHTENESS: this.number(100, 1) + '%' } },
-    number(max, min) { return Math.floor(Math.random() * (max - min + 1)) + min }
-}
+            case "b"://brake-held
+                switch (current_state) {
+                    case 1: current_state = 3; console.log('Off -> hold'); break;//Off -> hold
+                    case 2: current_state = 3; console.log('ignition-on -> hold'); break;//ignition-on -> hold
+                    case 6: //in-reverse-motion -> stationary position if speed = 0 | speed--
+                        /* IMPLIMENT SPEED */
+                        if (speed > 1) {
+                            speed--;
+                            console.log('in-reverse-motion slow to speed: ', speed)
+                        } else {
+                            current_state = 5;
+                            console.log('in-reverse-motion -> stationary position')
+                        }
+                        break;
+                    case 7: //in-forward-motion -> stationary position if speed = 0 | speed--
+                        /* IMPLIMENT SPEED */
+                        if (speed > 1) {
+                            speed--;
+                            console.log('in-forward-motion slow to speed: ', speed)
+                        } else {
+                            current_state = 5;
+                            console.log('in-forward-motion -> stationary position')
+                        }
+                        break;
+                    case 8://cruise-control-engaged -> in-forward-motion
+                        current_state = 7;
+                        console.log('cruise-control-engaged -> in-forward-motion')
+                        break;
+                    default:
+                        console.warn(states[current_state], ' no action can be taken with signal "b"')
+                        failiure(states[current_state], "b")
+                        return -1;
+                }
+                break;
 
-function linkify(text) {//Detects links in text
-    //https://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
-    var urlRegex = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;/*/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig*/;
-    return text.replace(urlRegex, function (url) {//returns text with imbeded html links
-        return '<a href="' + url + '">' + url + '</a>';
-    });
-}
+            case "c"://seat-belt-engaged
+                if (current_state == 4) {
+                    current_state = 5;
+                    console.log('cengine-started -> stationary position')
+                } else {
+                    console.warn(states[current_state], ' no action can be taken with signal "c"');
+                    failiure(states[current_state], "c")
+                    return -1;
+                }
+                break;
 
-//  post and get functions
-async function request(what) {//make a request to server for data
-
-    try {
-        var xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = function () {//wait for and handle response
-            if (this.readyState == 4 && this.status == 200) {
-                console.log('Server replied with: ', this.responseText, ' In response: ', this.response)
-                return this.responseText
-            }
-        };
-
-        xhttp.open("GET", what, true);//get request
-        xhttp.send();
-    } catch (err) {
-        console.warn('xhttp request failed ', err);
-    }
-
-}
-
-async function post(what, where) {//post data to server
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {//wait for and handle response
-        if (this.readyState == 4 && this.status == 200) {
-            console.log('Server replied with: ', this.responseText, ' In response: ', this.response)
+            case "d"://drive-selected
+                if (current_state == 5) {
+                    current_state = 7;
+                    console.log('stationary position  -> in-forward-motion')
+                } else {
+                    console.warn(states[current_state], ' no action can be taken with signal "d"');
+                    failiure(states[current_state], "d")
+                    return -1;
+                }
+                break;
+            case "e"://park-selected
+                if (current_state == 5) {
+                    current_state = 4;
+                    console.log('stationary position  -> engine-started')
+                } else {
+                    console.warn(states[current_state], ' no action can be taken with signal "e"');
+                    failiure(states[current_state], "e")
+                    return -1;
+                }
+                break;
+            case "f"://accelerate
+                switch (current_state) {
+                    case 5:// stationary position accelerate
+                        console.log('stationary position accelerate');
+                        break;
+                    case 6: //in-reverse-motion accelerate
+                        /* IMPLIMENT SPEED */
+                        speed++;
+                        console.log('in-reverse-motion accelerate to speed: ', speed);
+                        break;
+                    case 7: //in-forward-motion accelerate
+                        /* IMPLIMENT SPEED */
+                        speed++;
+                        console.log('in-forward-motion accelerate to speed: ', speed);
+                        break;
+                    case 8://cruise-control-engaged accelerate
+                        console.log('cruise-control-engaged accelerate');
+                        break;
+                    default:
+                        console.warn(states[current_state], ' no action can be taken with signal "f"')
+                        failiure(states[current_state], "f")
+                        return -1;
+                }
+                break;
+            case "g"://reverse-selected
+                if (current_state == 5) {
+                    current_state = 6;
+                    console.log('stationary position  -> in-reverse-motion')
+                } else {
+                    console.warn(states[current_state], ' no action can be taken with signal "g"');
+                    failiure(states[current_state], "g")
+                    return -1;
+                }
+                break;
+            case "i"://set-cruise-control
+                if (current_state == 7) {
+                    current_state = 8;
+                    console.log('in-forward-motion  -> cruise-control-engaged')
+                } else {
+                    console.warn(states[current_state], ' no action can be taken with signal "g"');
+                    failiure(states[current_state], "g")
+                    return -1;
+                }
+                break;
+            default:
+                console.warn(' Input/signal: ', inptarray[i], ' is not appart of the language')
+                failiure(states[current_state], inptarray[i])
+                return -1;
         }
-    };
+        stateout.innerText = states[current_state];
+    }
+}
 
-    xhttp.open("POST", where, true);
-    xhttp.send(JSON.stringify(what));
+function failiure(where, signal) {
+    console.warn('Failiure cannot apply ', signal, ' to state ', where)
 }
